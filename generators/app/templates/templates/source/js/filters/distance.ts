6e69a4c00095981de3b346b3481da6064d32c5a9
Gary Bortosky <garybort@gmail.com>
2015-11-01 01:18:46 -0500

    delete templates

--- generators/app/templates/templates/source/js/filters/distance.ts
+++
@@ -1,6 +0,0 @@
-namespace <% appName.capital %> {
-    export function showTotalFun(amount: number, unit: string) {
-        return amount * 5;
-    }
-}
-App.filter("funFilter", () => <% appName.capital %>.showTotalFun);
